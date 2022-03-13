c7c4c93beca5fe21fa3a9bc5a357d14023cf7883
Mahfidiitju <77566401+Mahfidiitju@users.noreply.github.com>
2022-03-13 19:47:55 +0600

    Create menu.php

---
+++ admin/repeat/menu.php
@@ -0,0 +1,26 @@
+<?php include('connection.php');
+include('login_check.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin panel</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <div class="menu">
+        <div class="box">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="admin1.php">Admin</a></li>
+                <li><a href="category.php">Category</a></li>
+                <li><a href="order.php">Order</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>  
+    </div>
